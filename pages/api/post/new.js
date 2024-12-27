d6299acd1747d5996df6e7a6a6890654b9ab955c
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2024-12-27 21:28:32 +0900

    api2

--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -1,7 +1,13 @@
 import prisma from '@/util/database';
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method == 'POST') {
-    console.log(req.body);
+    const post = await prisma.post.create({
+      data: {
+        title: req.body.title,
+        subtitle: req.body.subtitle,
+        content: req.body.content,
+      },
+    });
     return res.status(200).json('글좀 잘 쓰자');
   }
 }

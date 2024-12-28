cfa678fcc039ad6d38621e30bdcb694021a6d5a7
jungdadak <junghyun21110@gmail.com>
2024-12-29 01:56:06 +0900

    'hi'

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		domains: ["avatars.githubusercontent.com"], // 외부 이미지를 허용할 도메인 추가
+	},
+};
 
 export default nextConfig;

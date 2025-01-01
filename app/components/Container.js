fff4bf57bcdeeebec24c8f80eafa6e8a7ba62850
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2025-01-01 18:31:57 +0900

    mobile_padding

--- app/components/Container.js
+++ app/components/Container.js
@@ -1,11 +1,11 @@
 // components/Container.js
 export default function Container({ children }) {
-	return (
-		<div className="grid grid-cols-1 md:grid-cols-5 gap-4">
-			{/* 좌측 여백 */}
-			<div className="hidden md:block md:col-span-1"></div>
-			{/* 메인 콘텐츠 */}
-			<div className="col-span-1 md:col-span-4">{children}</div>
-		</div>
-	);
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
+      {/* 좌측 여백 */}
+      <div className="hidden md:block md:col-span-1"></div>
+      {/* 메인 콘텐츠 */}
+      <div className="col-span-1 md:col-span-4">{children}</div>
+    </div>
+  );
 }

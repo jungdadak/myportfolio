32653e74ea65871cca4fc974cc204206bd2d8ae6
jungdadak <junghyun21110@gmail.com>
2025-01-04 21:40:57 +0900

    'auth'

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,3 @@
 //app/api/auth/[...nextauth]/route.ts
-import { handlers } from '../../../auth'; // Referring to the auth.ts we just created
+import { handlers } from "../../../auth"; // Referring to the auth.ts we just created
 export const { GET, POST } = handlers;
